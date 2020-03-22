aeffdb6292e6285fe63d53deebe839b2631da82a
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-03-22 16:30:05 +0000

    Added logics for Race Day.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 let raceNumber = Math.floor(Math.random() * 1000);
+let earlyRegister = false;
+let runnerAge = 21;
+
+if (runnerAge > 18) {
+  raceNumber += 1000;
+}
+
+if (runnerAge > 18 && earlyRegister) {
+  console.log(`Race will start at 9:30, your number is: ${raceNumber}`);
+} else if (runnerAge > 18 && !earlyRegister) {
+  console.log(`Race will start at 11:00, your number is: ${raceNumber}`);
+} else if (runnerAge < 18) {
+  console.log(`Race will start at 12:00, your number is: ${raceNumber}`);
+} else {
+  console.log("Please see registration desk...");
+}
